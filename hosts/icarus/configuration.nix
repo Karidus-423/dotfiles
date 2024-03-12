2f320820a10d5ab5cba6406d4d98f0a4e11d60d1
Karidus-423 <kapuerto23@gmail.com>
2024-03-11 20:52:18 -0400

    bluetooth

--- hosts/icarus/configuration.nix
+++ hosts/icarus/configuration.nix
@@ -21,6 +21,10 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
   # Set your time zone.
   time.timeZone = "America/New_York";
 
