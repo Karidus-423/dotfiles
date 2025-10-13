df88965c63a93216007463e32989c832e366cce8
Karidus-423 <kapuerto23@gmail.com>
2025-10-13 16:00:49 -0400

    fixed reader to show name instead of whole path and using waybar again

--- .config/rofi/scripts/reader.sh
+++ .config/rofi/scripts/reader.sh
@@ -5,12 +5,14 @@ ROFI_FB_GENERIC_FO="xdg-open"
 selected=$(find ~/notes/Resources -type f)
 
 if [ "$@" ]; then
-	coproc ( "${ROFI_FB_GENERIC_FO}" "$@" >/dev/null 2>&1 )
+	file_path=$(find ~/notes/Resources -type f -name $@)
+	coproc ( "${ROFI_FB_GENERIC_FO}" "$file_path" >/dev/null 2>&1 )
 	exit 0
 fi
 
 echo -e "$selected" | while IFS= read -r file; do
-    echo "$file"
+	file_name=$(basename "$file") 
+    echo "$file_name"
 done
 
 
